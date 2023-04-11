d94ad64202fa1e592b67adc25d68a9eca8dd9e45
BarraR3port <barrar3port@gmail.com>
2023-04-11 16:03:53 -0400

    Avances en el trabajo

--- tarea-1/trabajo.c
+++ tarea-1/trabajo.c
@@ -49,14 +49,48 @@ void imprimirLista(LISTA lista) {
     }
 }
 
+void leeArchivo() {
+    char nombreArchivo[30], nombreArchivoSalida[30], linea[150];
+    printf("Ingresa el nombre del archivo: ");
+
+    fgets(nombreArchivo, 50, stdin);
+    strtok(nombreArchivo, "\n");
+
+    FILE *archivo = fopen(nombreArchivo, "r");
+
+    if (archivo == NULL) {
+        printf("Archivo invalido o no encontrado, cerrando el programa.");
+        exit(-1);
+    }
+
+    strcpy(nombreArchivoSalida, nombreArchivo);
+    strtok(nombreArchivoSalida, ".");
+    strcat(nombreArchivoSalida, ".sal");
+    FILE *archivoSalida = fopen(nombreArchivoSalida, "w");
+
+    while (fgets(linea, sizeof(linea), archivo)) {
+        if (linea[0] == "\n") continue;
+        char *rut;
+        char *nombre;
+        char *cadena;
+        int edad;
+        strtok(linea, "\n");
+        rut = strtok(linea, ",");
+        nombre = strtok(NULL, ",");
+        cadena = strtok(NULL, ",");
+        edad = atoi(cadena);
+        if (rut == NULL || nombre == NULL || edad == NULL) continue;
+        printf("Rut: %11s nombre: %20s             edad: %i\n", rut, nombre, edad);
+        fprintf(archivoSalida, "Rut: %11s nombre: %20s             edad: %i\n", rut, nombre, edad);
+
+    }
+
+    fclose(archivo);
+    fclose(archivoSalida);
+}
+
 void procesoPrincipal() {
     LISTA L = insertarAlFinalDeLaLista(NULL, "Bruno Bastidas", 5, "21424401-2");
-    insertarAlFinalDeLaLista(L, "Juan Perez", 3, "21424401-2");
-    insertarAlFinalDeLaLista(L, "Pedro Martinez", 2, "21424401-2");
-    insertarAlFinalDeLaLista(L, "Maria Gonzalez", 1, "21424401-2");
-    insertarAlFinalDeLaLista(L, "Juliam Sanchez", 4, "21424401-2");
-    insertarAlFinalDeLaLista(L, "Pericia aria Gonzalez", 1, "21424401-2");
-    insertarAlFinalDeLaLista(L, "Noacho Sanchez", 4, "21424401-2");
 
     imprimirLista(L);
 }
