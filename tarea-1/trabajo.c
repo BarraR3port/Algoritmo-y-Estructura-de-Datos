b280dc6e26276720ad09411f86e1c05bc581f045
BarraR3port <barrar3port@gmail.com>
2023-04-14 09:53:30 -0400

    Avances en el trabajo

--- tarea-1/trabajo.c
+++ tarea-1/trabajo.c
@@ -41,21 +41,53 @@ LISTA insertarAlFinalDeLaLista(LISTA L, char nombre[30], int numEntradas, char r
 void imprimirLista(LISTA lista) {
     LISTA aux = lista;
     int i = 0;
+    printf("\n");
     while (aux != NULL) {
-        printf("AUX = %i | Nombre: %s Edad: %i siguiente-> %s\n", i, aux->nombre, aux->entradas,
+        printf("AUX = %i | Nombre: %s Entradas: %i siguiente-> %s\n", i, aux->nombre, aux->entradas,
                !aux->siguiente ? "NULL" : aux->siguiente->nombre);
         i++;
         aux = aux->siguiente;
     }
 }
 
+int vecesRepetidas(LISTA lista, tNodo *nodo) {
+    int veces = 0;
+    LISTA aux = lista;
+    while (aux != NULL) {
+        if (strcmp(aux->rut, nodo->rut) == 0 ) {
+            veces = veces + aux->entradas;
+        }
+        aux = aux->siguiente;
+    }
+    return veces;
+}
+
+LISTA retornarEntradasUnicas(LISTA listaInicial){
+    LISTA aux = listaInicial;
+    LISTA listaUnica = NULL;
+    while (aux != NULL) {
+        int repetidas  = vecesRepetidas(listaInicial, aux);
+        if ( repetidas<=2 ) {
+            if (listaUnica == NULL){
+                listaUnica = aux;
+            } else {
+                listaUnica->siguiente = aux;
+            }
+        }
+        //printf("Veces repetidas de RUT: %s es: %i\n", aux->rut, repetidas);
+        aux = aux->siguiente;
+    }
+    return listaUnica;
+}
+
+
 void leeArchivo() {
     char nombreArchivo[30], nombreArchivoSalida[30], linea[150];
     printf("Ingresa el nombre del archivo: ");
 
-    fgets(nombreArchivo, 50, stdin);
-    strtok(nombreArchivo, "\n");
-
+    //fgets(nombreArchivo, 50, stdin);
+    //strtok(nombreArchivo, "\n");
+    strcpy(nombreArchivo, "datos1.txt");
     FILE *archivo = fopen(nombreArchivo, "r");
 
     if (archivo == NULL) {
@@ -68,31 +100,36 @@ void leeArchivo() {
     strcat(nombreArchivoSalida, ".sal");
     FILE *archivoSalida = fopen(nombreArchivoSalida, "w");
 
+    LISTA lista = NULL;
     while (fgets(linea, sizeof(linea), archivo)) {
         if (linea[0] == "\n") continue;
         char *rut;
         char *nombre;
         char *cadena;
-        int edad;
+        int entradas;
         strtok(linea, "\n");
         rut = strtok(linea, ",");
         nombre = strtok(NULL, ",");
         cadena = strtok(NULL, ",");
-        edad = atoi(cadena);
-        if (rut == NULL || nombre == NULL || edad == NULL) continue;
-        printf("Rut: %11s nombre: %20s             edad: %i\n", rut, nombre, edad);
-        fprintf(archivoSalida, "Rut: %11s nombre: %20s             edad: %i\n", rut, nombre, edad);
-
+        entradas = atoi(cadena);
+        //printf("Rut: %11s nombre: %20s             entradas: %i\n", rut, nombre, entradas);
+        if (rut == NULL || nombre == NULL ) continue;
+        lista = insertarAlFinalDeLaLista(lista, nombre, entradas, rut);
+        /*write data inside the sal file*/
+        fprintf(archivoSalida, "Rut: %11s nombre: %20s             entradas: %i \n", rut, nombre, entradas);
     }
+   // imprimirLista(lista);
+    LISTA entradasUnicas = retornarEntradasUnicas(lista);
+
+    imprimirLista(entradasUnicas);
+
 
     fclose(archivo);
     fclose(archivoSalida);
 }
 
 void procesoPrincipal() {
-    LISTA L = insertarAlFinalDeLaLista(NULL, "Bruno Bastidas", 5, "21424401-2");
-
-    imprimirLista(L);
+    leeArchivo();
 }
 
 int main() {
