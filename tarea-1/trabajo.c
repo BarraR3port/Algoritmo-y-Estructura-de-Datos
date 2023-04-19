2066b8aafed6dc0d4cc95f384cfbeee2d5cbde4e
BarraR3port <barrar3port@gmail.com>
2023-04-19 17:42:12 -0400

    95% del trabajo, falta documentar.

--- tarea-1/trabajo.c
+++ tarea-1/trabajo.c
@@ -1,152 +1,106 @@
-//
-// Created by barra on 10-04-2023.
-//
-#include<stdio.h>
+#include <stdio.h>
 #include <malloc.h>
-#include<string.h>
+#include <string.h>
 
-struct usuario {
+struct usuario
+{
     int entradasPedidas;
-    int entradasOtorgadas;
+    int entradasAsignadas;
     char nombre[30];
     char rut[11];
     struct usuario *siguiente;
 };
 
-
 typedef struct usuario tUsuario;
 
-typedef tUsuario *LISTA;
+typedef tUsuario *ListaUsuarios;
 
-struct entradas {
-    LISTA vendidas;
-    LISTA declinadas;
+struct entradas
+{
+    ListaUsuarios vendidas;
+    ListaUsuarios declinadas;
+    ListaUsuarios pendientes;
+    int disponibles;
 };
 
 typedef struct entradas tEntradas;
 
-typedef tEntradas *ENTRADAS;
+typedef tEntradas *EntradasUsuarios;
 
-int ENTRADAS_DISPONIBLES = 0;
+char nombreArchivo[30];
 
-LISTA crearNodo(char nombre[30], int numEntradas, char rut[11], struct usuario *siguiente, int entradasOtorgadas) {
-    LISTA aux;
+ListaUsuarios crearListaUsuarios(char nombre[30], int numEntradas, char rut[11], struct usuario *siguiente, int entradasAsignadas)
+{
+    ListaUsuarios aux;
     aux = malloc(sizeof(tUsuario));
     strcpy(aux->rut, rut);
     strcpy(aux->nombre, nombre);
     aux->entradasPedidas = numEntradas;
-    aux->entradasOtorgadas = entradasOtorgadas;
+    aux->entradasAsignadas = entradasAsignadas;
     aux->siguiente = siguiente;
+    return aux;
 }
 
-LISTA insertarAlFinalDeLaLista(LISTA L, char nombre[30], int numEntradas, char rut[11], int numEntradasOtorgadas) {
-    if (L == NULL) {
-        return crearNodo(nombre, numEntradas, rut, NULL,numEntradasOtorgadas);
+ListaUsuarios insertarAlFinalDeLaLista(ListaUsuarios lista, char nombre[30], int entradasPedidas, char rut[11], int entradasAsignadas)
+{
+    if (lista == NULL)
+    {
+        return crearListaUsuarios(nombre, entradasPedidas, rut, NULL, entradasAsignadas);
     }
-    LISTA aux;
-    aux = L;
-    while (aux->siguiente != NULL) {
+    ListaUsuarios aux;
+    aux = lista;
+    while (aux->siguiente != NULL)
+    {
         aux = aux->siguiente;
     }
-    aux->siguiente = crearNodo(nombre, numEntradas, rut, NULL,numEntradasOtorgadas);
-    return L;
+    aux->siguiente = crearListaUsuarios(nombre, entradasPedidas, rut, NULL, entradasAsignadas);
+    return lista;
 }
 
-void imprimirLista(LISTA lista) {
-    LISTA aux = lista;
-    int i = 0;
-    printf("\n");
-    while (aux != NULL) {
-        printf("AUX = %i | Nombre: %s Entradas: %i rut: %s\n", i, aux->nombre, aux->entradasPedidas, aux->rut);
-        i++;
-        aux = aux->siguiente;
-    }
-    free(aux);
-}
-
-int existe(LISTA lista, char rut[11]) {
-    LISTA aux = lista;
-    int counts = 0;
-    while (aux != NULL && rut != NULL) {
-        if (strcmp(aux->rut, rut) == 0){
-            counts++;
+int existe(ListaUsuarios lista, char rut[11])
+{
+    ListaUsuarios aux = lista;
+    while (aux != NULL)
+    {
+        if (strcmp(aux->rut, rut) == 0)
+        {
+            return 0;
         }
-        if (counts >= 2) return 0;
         aux = aux->siguiente;
     }
-    free(aux);
     return 1;
 }
 
-LISTA agregarEntradas(LISTA lista, char rut[11], int entradas) {
-    LISTA aux = lista;
-    while (aux != NULL) {
-        if (strcmp(aux->rut, rut) == 0) {
-            aux-> entradasPedidas = aux->entradasPedidas + entradas;
+ListaUsuarios agregarEntradas(ListaUsuarios lista, char rut[11], int entradas)
+{
+    ListaUsuarios aux = lista;
+    while (aux != NULL)
+    {
+        if (strcmp(aux->rut, rut) == 0)
+        {
+            aux->entradasPedidas = aux->entradasPedidas + entradas;
         }
         aux = aux->siguiente;
     }
     return lista;
 }
 
-LISTA limpiarYReducirLista(LISTA listaOriginal){
-    LISTA aux = listaOriginal;
-    LISTA nuevaLista = NULL;
-    imprimirLista(listaOriginal);
-    while (aux != NULL) {
-        int existeEnLaLista = existe(nuevaLista , aux->rut);
-        if ( existeEnLaLista == 0 ){
-            nuevaLista = agregarEntradas(nuevaLista, aux->rut, aux->entradasPedidas);
-        } else {
-            nuevaLista = insertarAlFinalDeLaLista(nuevaLista, aux->nombre, aux->entradasPedidas, aux->rut, aux->entradasOtorgadas);
-        }
-        printf("\nExiste: %s rut: %s : %s", aux->nombre,aux->rut, existeEnLaLista == 0 ? "Si" : "No");
-
-        aux = aux->siguiente;
-    }
-    printf("\nNueva lista: ");
-    imprimirLista(nuevaLista);
-    return nuevaLista;
-}
-
-ENTRADAS asignarEntradas(ENTRADAS entradas, LISTA lista) {
-    LISTA nuevaLista = limpiarYReducirLista(lista);
-    while (nuevaLista != NULL) {
-        if (nuevaLista->entradasPedidas <= 2) {
-            entradas->vendidas = insertarAlFinalDeLaLista(entradas->vendidas, nuevaLista->nombre, nuevaLista->entradasPedidas, nuevaLista->rut, nuevaLista->entradasPedidas);
-        } else {
-            entradas->declinadas = insertarAlFinalDeLaLista(entradas->declinadas, nuevaLista->nombre, nuevaLista->entradasPedidas, nuevaLista->rut, nuevaLista->entradasPedidas);
-        }
-        nuevaLista = nuevaLista->siguiente;
-    }
-    /*printf("\nVendidas: ");
-    imprimirLista(entradas->vendidas);
-    printf("Declinadas: ");
-    imprimirLista(entradas->declinadas);*/
-    return entradas;
-}
-
-void leeArchivo() {
-    char nombreArchivo[30], nombreArchivoSalida[30], linea[150];
+ListaUsuarios leeArchivo()
+{
+    char linea[150];
     printf("Ingresa el nombre del archivo: ");
-
-    //fgets(nombreArchivo, 50, stdin);
-    //strtok(nombreArchivo, "\n");
-    strcpy(nombreArchivo, "datos1.txt");
+    strcpy(nombreArchivo, "datos2.txt");
     FILE *archivo = fopen(nombreArchivo, "r");
 
-    if (archivo == NULL) {
+    if (archivo == NULL)
+    {
         printf("Archivo invalido o no encontrado, cerrando el programa.");
         exit(-1);
     }
 
-    strcpy(nombreArchivoSalida, nombreArchivo);
-    strtok(nombreArchivoSalida, ".");
-    strcat(nombreArchivoSalida, ".sal");
-    FILE *archivoSalida = fopen(nombreArchivoSalida, "w");
-
-    LISTA lista = NULL;
-    while (fgets(linea, sizeof(linea), archivo)) {
+    ListaUsuarios lista = NULL;
+    while (fgets(linea, sizeof(linea), archivo))
+    {
         char *rut;
         char *nombre;
         char *cadena;
@@ -156,28 +110,133 @@ void leeArchivo() {
         nombre = strtok(NULL, ",");
         cadena = strtok(NULL, ",");
         entradas = atoi(cadena);
-        if (rut == NULL || nombre == NULL) continue;
-        lista = insertarAlFinalDeLaLista(lista, nombre, entradas, rut,0);
-        fprintf(archivoSalida, "Rut: %11s nombre: %20s             entradas: %i \n", rut, nombre, entradas);
+        if (rut == NULL || nombre == NULL)
+        {
+            continue;
+        }
+        lista = insertarAlFinalDeLaLista(lista, nombre, entradas, rut, 0);
     }
 
-    ENTRADAS entradas = malloc(sizeof(ENTRADAS));
-    entradas = asignarEntradas(entradas, lista);
-    // imprimirLista(lista);
-    // LISTA entradasUnicas = retornarEntradasUnicas(lista);
-
-    //imprimirLista(entradasUnicas);
-
-
     fclose(archivo);
-    fclose(archivoSalida);
+    return lista;
 }
 
-void procesoPrincipal() {
-    leeArchivo();
+ListaUsuarios retornarEntradasUnicas(ListaUsuarios lista)
+{
+    ListaUsuarios aux = lista;
+    ListaUsuarios listaUnica = NULL;
+    while (aux != NULL)
+    {
+        if (existe(listaUnica, aux->rut))
+        {
+            listaUnica = insertarAlFinalDeLaLista(listaUnica, aux->nombre, aux->entradasPedidas, aux->rut, aux->entradasAsignadas);
+        }
+        else
+        {
+            listaUnica = agregarEntradas(listaUnica, aux->rut, aux->entradasPedidas);
+        }
+        aux = aux->siguiente;
+    }
+    return listaUnica;
 }
 
-int main() {
+EntradasUsuarios retornarEntradasYAsignarEntradas(ListaUsuarios lista)
+{
+    ListaUsuarios aux = lista;
+    ListaUsuarios vendidas = NULL;
+    ListaUsuarios declinadas = NULL;
+    ListaUsuarios pendientes = NULL;
+    int entradasDisponibles = 50;
+    while (aux != NULL)
+    {
+        if (aux->entradasPedidas <= 5 && aux->entradasPedidas > 0)
+        {
+            if (aux->entradasPedidas >= 3 && aux->entradasPedidas <= 5)
+            {
+                if (entradasDisponibles - aux->entradasPedidas <= 0)
+                {
+                    declinadas = insertarAlFinalDeLaLista(declinadas, aux->nombre, aux->entradasPedidas, aux->rut, 0);
+                }
+                else
+                {
+                    vendidas = insertarAlFinalDeLaLista(vendidas, aux->nombre, aux->entradasPedidas, aux->rut, 2);
+                    entradasDisponibles = entradasDisponibles - 2;
+                }
+            }
+            else
+            {
+                if (entradasDisponibles - aux->entradasPedidas < 0)
+                {
+                    pendientes = insertarAlFinalDeLaLista(pendientes, aux->nombre, aux->entradasPedidas, aux->rut, 0);
+                }
+                else
+                {
+                    vendidas = insertarAlFinalDeLaLista(vendidas, aux->nombre, aux->entradasPedidas, aux->rut, aux->entradasPedidas);
+                    entradasDisponibles = entradasDisponibles - aux->entradasPedidas;
+                }
+            }
+        }
+        else
+        {
+            declinadas = insertarAlFinalDeLaLista(declinadas, aux->nombre, aux->entradasPedidas, aux->rut, 0);
+        }
+        aux = aux->siguiente;
+    }
+    EntradasUsuarios entradas = malloc(sizeof(tEntradas));
+    entradas->vendidas = vendidas;
+    entradas->declinadas = declinadas;
+    entradas->pendientes = pendientes;
+    entradas->disponibles = entradasDisponibles;
+
+    return entradas;
+}
+
+void guardarArchivos(EntradasUsuarios entradas)
+{
+
+    char nombreArchivoEntradasAsignadas[30], nombreArchivoEntradasPendientes[30];
+
+    strcpy(nombreArchivoEntradasAsignadas, nombreArchivo);
+    strcpy(nombreArchivoEntradasPendientes, nombreArchivo);
+    strtok(nombreArchivoEntradasAsignadas, ".");
+    strtok(nombreArchivoEntradasPendientes, ".");
+    strcat(nombreArchivoEntradasAsignadas, ".asg");
+    strcat(nombreArchivoEntradasPendientes, ".esp");
+    FILE *archivoEntradasAsignadas = fopen(nombreArchivoEntradasAsignadas, "w");
+    FILE *archivoEntradasEnEspera = fopen(nombreArchivoEntradasPendientes, "w");
+
+    ListaUsuarios aux = entradas->vendidas;
+    while (aux != NULL)
+    {
+        fprintf(archivoEntradasAsignadas, "%s,%s,%i\n", aux->rut, aux->nombre, aux->entradasAsignadas);
+        aux = aux->siguiente;
+    }
+    fprintf(archivoEntradasAsignadas, "\nAsignadas: %i: \n", 50 - entradas->disponibles);
+    fprintf(archivoEntradasAsignadas, "Pendientes: %i: \n", entradas->disponibles);
+
+    aux = entradas->pendientes;
+    while (aux != NULL)
+    {
+        fprintf(archivoEntradasEnEspera, "%s,%s\n", aux->rut, aux->nombre);
+        aux = aux->siguiente;
+    }
+
+    fclose(archivoEntradasEnEspera);
+    fclose(archivoEntradasAsignadas);
+    free(entradas);
+    free(aux);
+}
+
+void procesoPrincipal()
+{
+    ListaUsuarios usuarios = leeArchivo();
+    ListaUsuarios lista = retornarEntradasUnicas(usuarios);
+    EntradasUsuarios entradas = retornarEntradasYAsignarEntradas(lista);
+    guardarArchivos(entradas);
+}
+
+int main()
+{
     procesoPrincipal();
     return 0;
 }
