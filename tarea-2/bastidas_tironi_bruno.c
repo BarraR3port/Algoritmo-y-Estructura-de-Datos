d10ba3be384205dd8026dc4a82b7bf1f184d5919
BarraR3port <barrar3port@gmail.com>
2023-06-05 02:24:02 -0400

    Terminado el codigo de la tarea2.

--- tarea-2/bastidas_tironi_bruno.c
+++ tarea-2/bastidas_tironi_bruno.c
@@ -3,30 +3,29 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MAX_NODES 10000
-#define infinito 10000000
-
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_RESET "\x1b[0m"
-
 typedef struct Usuario
 {
     int id;
-    char nombre[20];
+    char nombre[25];
     struct Usuario *siguiente;
 } Usuario;
 
-int numNodos, numArcos; // Numero de Personas y Numero de Relaciones
+int numNodos, numArcos;
 int **Grafo;
 bool *Visitados;
-int *Predecesor;
-int *Costo;
 int *Camino;
 Usuario *usuarios;
 int Pos = 0;
 bool CaminoTerminado = false;
 
-void agregarUsuariosALaCola(char nombre[25], int id)
+/*
+ * Entrada: nombre - El nombre del usuario a agregar.
+ *          id - El ID del usuario a agregar.
+ * Salida:  No retorna nada.
+ * Proceso: Crea un nuevo nodo de Usuario con el nombre y ID dados,
+ *          y lo agrega a la cola de usuarios existentes.
+ */
+void agregarUsuariosALaCola(char *nombre, int id)
 {
     Usuario *nuevoUsuario = malloc(sizeof(Usuario));
     nuevoUsuario->id = id;
@@ -47,15 +46,27 @@ void agregarUsuariosALaCola(char nombre[25], int id)
         current->siguiente = nuevoUsuario;
     }
 }
+
+/*
+ * Entrada: No recibe nada.
+ * Salida:  No retorna nada.
+ * Proceso: Crea los arreglos Visitados y Camino para almacenar los datos
+ *          necesarios para el cálculo del camino entre usuarios.
+ */
 void creaArreglosParaElCamino()
 {
     Visitados = calloc(numNodos, sizeof(bool));
-    Costo = calloc(numNodos, sizeof(int));
-    Predecesor = calloc(numNodos, sizeof(int));
     Camino = calloc(numNodos, sizeof(int));
 }
 
-int obtenerNumeroPorNombre(char nombre[25])
+/*
+ * Entrada: nombre - El nombre del usuario a buscar.
+ * Salida:  El ID del usuario correspondiente al nombre especificado.
+ *          Si no se encuentra ningún usuario con el nombre dado, se retorna -1.
+ * Proceso: Busca en la lista de usuarios el nombre especificado y retorna
+ *          el ID del usuario correspondiente.
+ */
+int obtenerNumeroPorNombre(char *nombre)
 {
     Usuario *current = usuarios;
     while (current != NULL)
@@ -69,6 +80,13 @@ int obtenerNumeroPorNombre(char nombre[25])
     return -1;
 }
 
+/*
+ * Entrada: numUsuario - El ID del usuario a buscar.
+ * Salida:  El nombre del usuario correspondiente al ID especificado.
+ *          Si no se encuentra ningún usuario con el ID dado, se retorna NULL.
+ * Proceso: Busca en la lista de usuarios el ID especificado y retorna
+ *          el nombre del usuario correspondiente.
+ */
 char *obtenerNombrePorNumero(int numUsuario)
 {
     Usuario *current = usuarios;
@@ -83,32 +101,36 @@ char *obtenerNombrePorNumero(int numUsuario)
     return NULL;
 }
 
-int leerArchivoYCrearGrafo(char nombreArchivo[9])
+/*
+ * Entrada: numUsuario - El ID del usuario a buscar.
+ * Salida:  El nombre del usuario correspondiente al ID especificado.
+ *          Si no se encuentra ningún usuario con el ID dado, se retorna NULL.
+ * Proceso: Busca en la lista de usuarios el ID especificado y retorna
+ *          el nombre del usuario correspondiente.
+ */
+int leerArchivoYCrearGrafo(char *nombreArchivo)
 {
 
-    FILE *inputFile = fopen(nombreArchivo, "r");
+    FILE *archivo = fopen(nombreArchivo, "r");
 
-    if (inputFile == NULL)
+    if (archivo == NULL)
     {
         printf("Error al abrir el archivo.\n");
         return 1;
     }
 
-    fscanf(inputFile, "%d %d", &numNodos, &numArcos);
+    fscanf(archivo, "%d %d", &numNodos, &numArcos);
 
-    // Crear grafo
     Grafo = calloc(numNodos, sizeof(int *));
     for (int i = 0; i < numNodos; i++)
     {
         Grafo[i] = calloc(numNodos, sizeof(int));
     }
 
-    // Leer aristas y agregarlas al grafo
     for (int i = 0; i < numNodos; i++)
     {
         char nombreUsuario[25];
-        fscanf(inputFile, "%s", &nombreUsuario);
-        // printf("Numero: %i %s\n", i, nombreUsuario);
+        fscanf(archivo, "%s", &nombreUsuario);
         agregarUsuariosALaCola(nombreUsuario, i);
     }
 
@@ -116,20 +138,20 @@ int leerArchivoYCrearGrafo(char nombreArchivo[9])
     {
         char linea[50];
         char nombreUsuario1[25], nombreUsuario2[25];
-        fscanf(inputFile, "%s", &linea);
-        char *token = strtok(linea, ",");
-        if (token)
+        fscanf(archivo, "%s", &linea);
+        char *lineaActual = strtok(linea, ",");
+        if (lineaActual)
         {
-            strcpy(nombreUsuario1, token);
-            token = strtok(NULL, ",");
-            if (token)
+            strcpy(nombreUsuario1, lineaActual);
+            lineaActual = strtok(NULL, ",");
+            if (lineaActual)
             {
-                strtok(token, "\n");
-                strcpy(nombreUsuario2, token);
+                strtok(lineaActual, "\n");
+                strcpy(nombreUsuario2, lineaActual);
             }
         }
 
-        if (strcmp(nombreUsuario1, nombreUsuario2) != 0 && nombreUsuario1 != NULL && nombreUsuario2 != NULL)
+        if (strcmp(nombreUsuario1, nombreUsuario2) != 0)
         {
             int idUsuario1 = obtenerNumeroPorNombre(nombreUsuario1);
             int idUsuario2 = obtenerNumeroPorNombre(nombreUsuario2);
@@ -142,11 +164,17 @@ int leerArchivoYCrearGrafo(char nombreArchivo[9])
         }
     }
 
-    // Cerrar archivo de entrada y liberar memoria
-    fclose(inputFile);
+    fclose(archivo);
     return 0;
 }
 
+/*
+ * Entrada: numUsuario - El ID del usuario a buscar.
+ * Salida:  El nombre del usuario correspondiente al ID especificado.
+ *          Si no se encuentra ningún usuario con el ID dado, se retorna NULL.
+ * Proceso: Busca en la lista de usuarios el ID especificado y retorna
+ *          el nombre del usuario correspondiente.
+ */
 void GeneraCamino(int usuario1, int usuario2)
 {
     Visitados[usuario1] = true;
@@ -185,29 +213,33 @@ void GeneraCamino(int usuario1, int usuario2)
     }
 }
 
+/*
+ * Entrada: usuario1 - El ID del usuario de inicio.
+ *          usuario2 - El ID del usuario de destino.
+ * Salida:  No retorna nada.
+ * Proceso: Imprime el camino entre usuario1 y usuario2, si existe,
+ *          utilizando la información almacenada en el arreglo Camino.
+ */
 void ImprimeCaminos(int usuario1, int usuario2)
 {
-    // Restablecer los valores iniciales
     Pos = 0;
     CaminoTerminado = false;
-    memset(Visitados, false, numNodos * sizeof(bool));
-
-    // Revisar si son amigos o no:
-    if (Grafo[usuario1][usuario2] == 1)
+    for (int i = 0; i < numNodos; i++)
     {
-        printf("Los usuarios %s y %s son amigos.\n", obtenerNombrePorNumero(usuario1), obtenerNombrePorNumero(usuario2));
-        return;
+        Visitados[i] = false;
     }
-    else
+    char *nombreUsuario1 = obtenerNombrePorNumero(usuario1);
+    char *nombreUsuario2 = obtenerNombrePorNumero(usuario2);
+    if (Grafo[usuario1][usuario2] == 0)
     {
-        // Generar el camino
         GeneraCamino(usuario1, usuario2);
     }
-    if (!CaminoTerminado)
-    {
-        printf("No se encontró un camino entre los usuarios %s y %s.\n", obtenerNombrePorNumero(usuario1), obtenerNombrePorNumero(usuario2));
-    }
     else
+    {
+        printf("Los usuarios %s y %s son amigos.\n", nombreUsuario1, nombreUsuario2);
+        return;
+    }
+    if (CaminoTerminado)
     {
         printf("La secuencia de Nombres es: \n");
         for (int i = 0; i < Pos; i++)
@@ -222,8 +254,19 @@ void ImprimeCaminos(int usuario1, int usuario2)
         }
         printf("\n");
     }
+    else
+    {
+        printf("No se encontró un camino entre los usuarios %s y %s.\n", nombreUsuario1, nombreUsuario2);
+    }
 }
 
+/*
+ * Entrada: nodo1 - El ID del primer nodo a comparar.
+ *          nodo2 - El ID del segundo nodo a comparar.
+ * Salida:  El número de amigos en común entre los dos nodos.
+ * Proceso: Compara las relaciones de amistad entre nodo1 y nodo2
+ *          en el grafo y cuenta el número de amigos en común.
+ */
 int numAmigosEnComun(int nodo1, int nodo2)
 {
     int cont;
@@ -239,6 +282,12 @@ int numAmigosEnComun(int nodo1, int nodo2)
     return cont;
 }
 
+/*
+ * Entrada: nombreArchivo - El nombre del archivo de salida a crear.
+ * Salida:  0 si se crea el archivo correctamente, 1 si hay algún error.
+ * Proceso: Almacena en un archivo de salida la información del camino
+ *          generado entre dos usuarios, en el formato especificado.
+ */
 void almacenarArchivoSalida(char *nombreArchivo)
 {
     char nombreArchivoSalida[30];
@@ -274,33 +323,83 @@ void almacenarArchivoSalida(char *nombreArchivo)
             }
         }
     }
-
+    for (int i = 0; i < numNodos; i++)
+    {
+        free(GrafoSalida[i]);
+    }
+    free(GrafoSalida);
     fclose(archivoSalida);
 }
 
-int iniciarProceso()
+/*
+ * Entrada: No recibe nada.
+ * Salida:  No retorna nada.
+ * Proceso: Libera la memoria utilizada por el grafo, los usuarios, el camino
+ *          y los visitados.
+ */
+void liberarMemoria()
 {
-    if (leerArchivoYCrearGrafo("red2.txt") == 1)
-    {
-        return 1;
-    }
-    creaArreglosParaElCamino();
 
-    // Encontrar secuencia de nombres de personas utilizando Dijkstra
-    int inicio = obtenerNumeroPorNombre("Rocio"); // Índice del nodo de inicio
-    int destino = obtenerNumeroPorNombre("Luis"); // Índice del nodo de destino
-
-    ImprimeCaminos(inicio, destino);
-    almacenarArchivoSalida("red1.txt");
-    // Liberar memoria
     for (int i = 0; i < numNodos; i++)
     {
         free(Grafo[i]);
     }
     free(Grafo);
     free(usuarios);
+    free(Camino);
+    free(Visitados);
+}
+
+/*
+ * Entrada: No recibe nada.
+ * Salida:  0 si el proceso se completa correctamente, 1 si hay algún error.
+ * Proceso: Realiza el proceso principal del programa, que incluye leer el archivo
+ *          de entrada, crear el grafo, generar caminos, almacenar el archivo de salida
+ *          y liberar la memoria utilizada.
+ */
+int iniciarProceso()
+{
+    char nombreArchivo[30];
+    printf("Ingrese el nombre del archivo de entrada: ");
+    scanf("%s", nombreArchivo);
+
+    if (leerArchivoYCrearGrafo(nombreArchivo) == 1)
+    {
+        return 1;
+    }
+    creaArreglosParaElCamino();
+
+    printf("\nIngresa tu nombre: ");
+    char nombreUsuario[30], nombreUsuario2[30];
+    scanf("%s", nombreUsuario);
+    int inicio = obtenerNumeroPorNombre(nombreUsuario);
+    if (inicio == -1)
+    {
+        printf("El usuario %s no existe.\n", nombreUsuario);
+        return 1;
+    }
+    printf("A quien deseas conocer?: ");
+    scanf("%s", nombreUsuario2);
+    printf("\n");
+    int destino = obtenerNumeroPorNombre(nombreUsuario2);
+    if (destino == -1)
+    {
+        printf("El usuario %s no existe.\n", nombreUsuario2);
+        return 1;
+    }
+
+    ImprimeCaminos(inicio, destino);
+    almacenarArchivoSalida(nombreArchivo);
+    liberarMemoria();
     return 0;
 }
+
+/*
+ * Entrada: No recibe nada.
+ * Salida:  0 si el programa se ejecuta correctamente, 1 si hay algún error.
+ * Proceso: Función principal que se ejecuta al iniciar el programa.
+ *          Llama a la función iniciarProceso y devuelve su resultado.
+ */
 int main()
 {
     int proceso = iniciarProceso();
