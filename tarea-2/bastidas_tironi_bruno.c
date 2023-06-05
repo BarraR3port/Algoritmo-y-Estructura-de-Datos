cb99543f52745060b3c497ca9a736999c4e05b28
BarraR3port <barrar3port@gmail.com>
2023-06-05 03:15:52 -0400

    Ultimos arreglos del codigo.

--- tarea-2/bastidas_tironi_bruno.c
+++ tarea-2/bastidas_tironi_bruno.c
@@ -28,6 +28,7 @@ bool CaminoTerminado = false;
 void agregarUsuariosALaCola(char *nombre, int id)
 {
     Usuario *nuevoUsuario = malloc(sizeof(Usuario));
+
     nuevoUsuario->id = id;
     strcpy(nuevoUsuario->nombre, nombre);
     nuevoUsuario->siguiente = NULL;
@@ -39,6 +40,7 @@ void agregarUsuariosALaCola(char *nombre, int id)
     else
     {
         Usuario *current = usuarios;
+
         while (current->siguiente != NULL)
         {
             current = current->siguiente;
@@ -69,6 +71,7 @@ void creaArreglosParaElCamino()
 int obtenerNumeroPorNombre(char *nombre)
 {
     Usuario *current = usuarios;
+
     while (current != NULL)
     {
         if (strcmp(current->nombre, nombre) == 0)
@@ -90,6 +93,7 @@ int obtenerNumeroPorNombre(char *nombre)
 char *obtenerNombrePorNumero(int numUsuario)
 {
     Usuario *current = usuarios;
+
     while (current != NULL)
     {
         if (current->id == numUsuario)
@@ -122,6 +126,7 @@ int leerArchivoYCrearGrafo(char *nombreArchivo)
     fscanf(archivo, "%d %d", &numNodos, &numArcos);
 
     Grafo = calloc(numNodos, sizeof(int *));
+
     for (int i = 0; i < numNodos; i++)
     {
         Grafo[i] = calloc(numNodos, sizeof(int));
@@ -138,8 +143,10 @@ int leerArchivoYCrearGrafo(char *nombreArchivo)
     {
         char linea[50];
         char nombreUsuario1[25], nombreUsuario2[25];
+
         fscanf(archivo, "%s", &linea);
         char *lineaActual = strtok(linea, ",");
+
         if (lineaActual)
         {
             strcpy(nombreUsuario1, lineaActual);
@@ -180,6 +187,7 @@ void GeneraCamino(int usuario1, int usuario2)
     Visitados[usuario1] = true;
     Camino[Pos] = usuario1;
     Pos++;
+
     if (usuario1 == usuario2)
     {
         CaminoTerminado = true;
@@ -224,12 +232,15 @@ void ImprimeCaminos(int usuario1, int usuario2)
 {
     Pos = 0;
     CaminoTerminado = false;
+
     for (int i = 0; i < numNodos; i++)
     {
         Visitados[i] = false;
     }
+
     char *nombreUsuario1 = obtenerNombrePorNumero(usuario1);
     char *nombreUsuario2 = obtenerNombrePorNumero(usuario2);
+
     if (Grafo[usuario1][usuario2] == 0)
     {
         GeneraCamino(usuario1, usuario2);
@@ -246,6 +257,7 @@ void ImprimeCaminos(int usuario1, int usuario2)
         {
             int numero = Camino[i];
             char *nombre = obtenerNombrePorNumero(numero);
+
             if (nombre == NULL)
             {
                 continue;
@@ -269,10 +281,9 @@ void ImprimeCaminos(int usuario1, int usuario2)
  */
 int numAmigosEnComun(int nodo1, int nodo2)
 {
-    int cont;
-    int i;
-    cont = 0;
-    for (i = 0; i < numNodos; i++)
+    int cont = 0;
+
+    for (int i = 0; i < numNodos; i++)
     {
         if (Grafo[nodo1][i] != 0 && Grafo[nodo2][i] != 0)
         {
@@ -291,11 +302,13 @@ int numAmigosEnComun(int nodo1, int nodo2)
 void almacenarArchivoSalida(char *nombreArchivo)
 {
     char nombreArchivoSalida[30];
+
     strcpy(nombreArchivoSalida, nombreArchivo);
     strtok(nombreArchivoSalida, ".");
     strcat(nombreArchivoSalida, ".aee");
 
     FILE *archivoSalida = fopen(nombreArchivoSalida, "w");
+
     if (archivoSalida == NULL)
     {
         printf("Error al abrir el archivo de salida.\n");
@@ -303,13 +316,16 @@ void almacenarArchivoSalida(char *nombreArchivo)
     }
 
     int **GrafoSalida = calloc(numNodos, sizeof(int *));
+
     for (int i = 0; i < numNodos; i++)
     {
         GrafoSalida[i] = calloc(numNodos, sizeof(int));
     }
+
     for (int i = 0; i < numNodos; i++)
     {
         int amistades = 0;
+
         for (int j = 0; j < numNodos; j++)
         {
             if (Grafo[i][j] == 1 && GrafoSalida[i][j] == 0 && GrafoSalida[j][i] == 0)
@@ -323,10 +339,12 @@ void almacenarArchivoSalida(char *nombreArchivo)
             }
         }
     }
+
     for (int i = 0; i < numNodos; i++)
     {
         free(GrafoSalida[i]);
     }
+
     free(GrafoSalida);
     fclose(archivoSalida);
 }
@@ -344,6 +362,7 @@ void liberarMemoria()
     {
         free(Grafo[i]);
     }
+
     free(Grafo);
     free(usuarios);
     free(Camino);
@@ -360,6 +379,7 @@ void liberarMemoria()
 int iniciarProceso()
 {
     char nombreArchivo[30];
+
     printf("Ingrese el nombre del archivo de entrada: ");
     scanf("%s", nombreArchivo);
 
@@ -367,21 +387,27 @@ int iniciarProceso()
     {
         return 1;
     }
-    creaArreglosParaElCamino();
 
+    creaArreglosParaElCamino();
     printf("\nIngresa tu nombre: ");
+
     char nombreUsuario[30], nombreUsuario2[30];
+
     scanf("%s", nombreUsuario);
     int inicio = obtenerNumeroPorNombre(nombreUsuario);
+
     if (inicio == -1)
     {
         printf("El usuario %s no existe.\n", nombreUsuario);
         return 1;
     }
+
     printf("A quien deseas conocer?: ");
     scanf("%s", nombreUsuario2);
     printf("\n");
+
     int destino = obtenerNumeroPorNombre(nombreUsuario2);
+
     if (destino == -1)
     {
         printf("El usuario %s no existe.\n", nombreUsuario2);
